ea40582c8cccf0c0ce71aceb83fd1b915432b9a0
Daily-manjoo <yuslz35@gmail.com>
2023-06-19 16:50:47 +0900

    0619 commit

---
+++ pages/section02/02-01-counter-let-document/index.js
@@ -0,0 +1,13 @@
+export default function CounterLetDocumentPage() {
+  function onClickCountUp() {}
+
+  function onClickCountDown() {}
+
+  reutrn(
+    <div>
+      <div>0</div>
+      <button>카운트 올리기</button>
+      <button>카운트 내리기</button>
+    </div>
+  );
+}
