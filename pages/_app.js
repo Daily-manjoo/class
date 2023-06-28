cd7379c2b39a89c63cb489c18ea1807b483aa54c
Daily-manjoo <yuslz35@gmail.com>
2023-06-29 00:59:07 +0900

    app.js 파일 존재 이유

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 /*import "@/styles/globals.css";*/
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component }) {
   const client = new ApolloClient({
     uri: "http://backend-example.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(), //컴퓨터 메모리에다 백엔드에서 받아온 데이터 임시 저장해놓기 => 나중에 더 자세히 알아보기!
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
     <div>
       <div>===================여기는 _app.js 컴포넌트 시작 부분입니다. ==============================</div>
       <ApolloProvider client={client}>
-        <Component {...pageProps} />
+        <Component />
       </ApolloProvider>
       <div>===================여기는 _app.js 컴포넌트 마지막 부분입니다. ==============================</div>
     </div>
--- pages/section04/04-01-rest-get/index.js
+++ pages/section04/04-01-rest-get/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import 나만의페이지 from "../../section01/01-01-example";
 
 export default function RestGetPage() {
   function onClickAsync() {
@@ -6,20 +7,21 @@ export default function RestGetPage() {
     console.log(result); //Promise
   }
 
-//   async function onClickSync() {
-//     const result = await axios.get("https://koreanjson.com/posts/1");    => 함수 중복 선언 문제
-//     console.log(result); //제대로 된 결과
-//   }
+  //   async function onClickSync() {
+  //     const result = await axios.get("https://koreanjson.com/posts/1");    => 함수 중복 선언 문제
+  //     console.log(result); //제대로 된 결과
+  //   }
 
-    const onClickSync = async () =>  {
+  const onClickSync = async () => {
     const result = await axios.get("https://koreanjson.com/posts/1");
     console.log(result); //제대로 된 결 과
-  }
+  };
 
   return (
     <div>
       <button onClick={onClickAsync}>REST-API(비동기) 요청하기</button>
       <button onClick={onClickSync}>REST-API(동기) 요청하기</button>
+      <나만의페이지 />
     </div>
   );
 }
