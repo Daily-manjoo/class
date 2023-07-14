a8d94cc154a5f8a46fe199fc0586c4755a8c7fde
Daily-manjoo <yuslz35@gmail.com>
2023-07-15 03:10:09 +0900

    static-routing-board-query 게시물 가져오기

---
+++ pages/section05/05-03-static-routing-board-moved-query/1/index.js
@@ -0,0 +1,27 @@
+import { useQuery, gql } from "@apollo/client";
+
+const FETCH_BOARD = gql`
+  query {
+    fetchBoard(number: 39) {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data } = useQuery(FETCH_BOARD);
+
+  console.log(data);
+
+  return (
+    <div>
+      <div>39번 게시글 이동 완료!</div>
+      <div>작성자: {data?.fetchBoard.writer}</div>
+      <div>제목: {data?.fetchBoard.title}</div>
+      <div>내용: {data?.fetchBoard.contents}</div>
+    </div> //data가 있으면 그리고 없으면 그리지X *옵셔널체이닝
+  );
+}
