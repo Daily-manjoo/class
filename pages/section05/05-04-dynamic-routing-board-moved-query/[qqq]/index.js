d059f35be7fe38cf8e27d5c4f00c56e5a2bfde2f
Daily-manjoo <yuslz35@gmail.com>
2023-07-16 01:59:28 +0900

    dynamic-routing-board-query 동적 라우팅 게시판 조회

---
+++ pages/section05/05-04-dynamic-routing-board-moved-query/[qqq]/index.js
@@ -0,0 +1,33 @@
+import { useQuery, gql } from "@apollo/client";
+import { useRouter } from "next/router";
+
+//아래 mutation처럼 타입도 적어주기
+const FETCH_BOARD = gql`
+  query fetchBoard($number: Int) {
+    fetchBoard(number: $number) {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const router = useRouter();
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { number: Number(router.query.qqq) },
+  });
+
+  console.log(data);
+
+  return (
+    <div>
+      <div>{router.query.qqq}번 게시글 이동 완료!</div>
+      <div>작성자: {data?.fetchBoard?.writer}</div>
+      <div>제목: {data?.fetchBoard?.title}</div>
+      <div>내용: {data?.fetchBoard?.contents}</div>
+    </div> //data가 있으면 그리고 없으면 그리지X *옵셔널체이닝
+  );
+}
