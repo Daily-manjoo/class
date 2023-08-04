77fdfdd738c7a0b4c84212398bda5b4df406bbc0
Daily-manjoo <yuslz35@gmail.com>
2023-08-05 02:49:23 +0900

    set-state 원리

---
+++ pages/section07/07-01-set-state/index.js
@@ -0,0 +1,10 @@
+import BoardWrite from "@/src/component/units/board/07-write/BoardWrite.container";
+export default function GraphqlMutation() {
+  return (
+    <div>
+      <div>### 페이지 ###</div>
+      <BoardWrite qqq="철수" />
+      <div>### 페이지 ###</div>
+    </div>
+  );
+}
