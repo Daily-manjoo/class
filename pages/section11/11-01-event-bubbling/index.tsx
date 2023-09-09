7d616019165b740047fba1b45b0af9f4b29e841a
Daily-manjoo <yuslz35@gmail.com>
2023-09-09 17:12:23 +0900

    이벤트 버블링

---
+++ pages/section11/11-01-event-bubbling/index.tsx
@@ -0,0 +1,38 @@
+import { MouseEvent } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const FETCH_BOARDS = gql`
+  query {
+    fetchBoards {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data } = useQuery(FETCH_BOARDS);
+
+  console.log(data?.fetchBoards);
+
+  const onClickAlert = (event: MouseEvent<HTMLDivElement>) => {
+    alert(event.currentTarget.id);
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el: any) => (
+        <div id={el.writer} onClick={onClickAlert}>
+          <span>
+            <input type="checkbox" />
+          </span>
+          <span style={{ margin: "10px" }}>{el.number}</span>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+    </div> //data가 있으면 그리고 없으면 그리지X *옵셔널체이닝
+  );
+}
