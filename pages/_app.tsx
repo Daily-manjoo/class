075157e52f55515810c01b29aa1b8b691b0d6974
Daily-manjoo <yuslz35@gmail.com>
2023-09-17 00:58:48 +0900

    Global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@
 import type { AppProps } from "next/app";
 import Layout from "../src/component/commons/layout";
 import ApolloSetting from "../src/component/commons/apollo";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 
 export default function App({ Component }: AppProps): JSX.Element {
   return (
@@ -11,9 +13,12 @@ export default function App({ Component }: AppProps): JSX.Element {
         부분입니다.==============================
       </div>
       <ApolloSetting>
-        <Layout>
-          <Component />
-        </Layout>
+        <>
+          <Global styles={globalStyles} />
+          <Layout>
+            <Component />
+          </Layout>
+        </>
       </ApolloSetting>
       <div>
         ===================여기는 _app.js 컴포넌트 마지막 부분입니다.
