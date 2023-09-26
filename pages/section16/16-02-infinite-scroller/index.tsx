fb6c1e278f3f9a5fa87e6a0d301512a1bff9f085
Daily-manjoo <yuslz35@gmail.com>
2023-09-27 01:29:09 +0900

    infinite-scroll 구현

---
+++ pages/section16/16-02-infinite-scroller/index.tsx
@@ -0,0 +1,55 @@
+import { useQuery, gql } from "@apollo/client";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+import InfiniteScroll from "react-infinite-scroller";
+import React from "react";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data, fetchMore } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS); //앞 부분은 결과타입, 뒤에는 variables(인자) 타입
+
+  const onLoadMore = (): void => {
+    if (data === undefined) return;
+    void fetchMore({
+      variables: { page: Math.ceil((data?.fetchBoards.length ?? 10) / 10) + 1 },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult.fetchBoards === undefined) {
+          return {
+            fetchBoards: [...prev.fetchBoards],
+          };
+        }
+        return {
+          fetchBoards: [...prev.fetchBoards, ...fetchMoreResult.fetchBoards],
+        };
+      },
+    });
+  };
+
+  return (
+    <div>
+      <InfiniteScroll pageStart={0} loadMore={onLoadMore} hasMore={true}>
+        {data?.fetchBoards.map((el) => (
+          <div key={el._id}>
+            <span style={{ margin: "10px" }}>{el.title}</span>
+            <span style={{ margin: "10px" }}>{el.writer}</span>
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div> //data가 있으면 그리고 없으면 그리지X *옵셔널체이닝
+  );
+}
