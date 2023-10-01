f877a4929ca38deecc70ba0039ccfd7ab3961fb2
Daily-manjoo <yuslz35@gmail.com>
2023-10-01 23:36:50 +0900

    useEffect openapi에서 활용하기

---
+++ pages/section17/17-04-openapi-with-useEffect/index.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import 나만의페이지 from "../../section01/01-01-example";
+import { useEffect, useState } from "react";
+
+export default function RestGetPage(): JSX.Element {
+  const [dog, setDog] = useState();
+
+  useEffect(() => {
+    const onClickSync = async (): Promise<void> => {
+      const result = await axios.get("https://dog.ceo/api/breeds/image/random");
+      console.log(result.data.message); // 사진 주소
+      setDog(result.data.message);
+    };
+    void onClickSync();
+  }, []);
+
+  return (
+    <div>
+      <img src={dog} />
+      {/* <button onClick={onClickSync}>REST-API(동기) 요청하기</button> */}
+      <나만의페이지 />
+    </div>
+  );
+}
